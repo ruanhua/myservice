7a48cde179a657c81810d07d6654ac1370d79ec5
ruanhua <17701327981@163.com>
2022-02-06 19:04:13 +0800

    解决跨域问题

--- src/main/java/com/cn/myservice/controller/MenuController.java
+++ src/main/java/com/cn/myservice/controller/MenuController.java
@@ -7,6 +7,7 @@ import com.cn.myservice.service.IMenuService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,7 @@ public class MenuController {
 
     @Operation(summary = "获取树形菜单", description = "全量菜单权限")
     @GetMapping("/menuTree")
+    @CrossOrigin
     public R<List<Menu>> getResource() {
         return iMenuService.getAllMenuTree();
     }
